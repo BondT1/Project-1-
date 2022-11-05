5ee99a7043ca0bea0877ed604327451638958b40
Faiz Ahmed <faizahmed2002@gmail.com>
2022-11-05 17:00:51 +0000

    Formattedcode

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const searchBtn = document.querySelector('#search-button');
-const inputSearch = document.querySelector('#about-us-search')
-const mainEl = document.querySelector('#about-us')
+const searchBtn = document.querySelector("#search-button");
+const inputSearch = document.querySelector("#about-us-search");
+const mainEl = document.querySelector("#about-us");
 
 function getApi(event) {
-    let searchTerm = inputSearch.value
+  let searchTerm = inputSearch.value;
 
-      var requestUrl =  'https://api.unsplash.com/search/photos/?client_id=dtdf4YL-mhArMfnqr6Kff3o0ccg2LXWb6Q4bWkXEI4o&query='
-      +searchTerm
+  var requestUrl =
+    "https://api.unsplash.com/search/photos/?client_id=dtdf4YL-mhArMfnqr6Kff3o0ccg2LXWb6Q4bWkXEI4o&query=" +
+    searchTerm;
 
-    fetch(requestUrl)
+  fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        console.log(data)
+      console.log(data);
     });
 
-    mainEl.hidden =true
-    
+  mainEl.hidden = true;
 }
 
-searchBtn.addEventListener('click', getApi);
+searchBtn.addEventListener("click", getApi);
