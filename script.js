9c6f3dd98488b224cffa3313bde9655204d7ae32
nagibnur <nagibnur@hotmail.com>
2022-11-07 18:59:38 +0000

    fix some bugs, got the img link working

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-const searchBtn = document.querySelector("#search-button");
-const inputSearch = document.querySelector("#about-us-search");
-const mainEl = document.querySelector("#about-us");
+const searchBtn = document.querySelector('#search-button');
+const inputSearch = document.querySelector('#about-us-search')
+const mainEl = document.querySelector('#about-us')
 
-function getApi(event) {
-  let searchTerm = inputSearch.value;
+searchBtn.addEventListener('click', submitHandler)
 
-  var requestUrl =
-    "https://api.unsplash.com/search/photos/?client_id=dtdf4YL-mhArMfnqr6Kff3o0ccg2LXWb6Q4bWkXEI4o&query=" +
-    searchTerm;
+function submitHandler(e) {
+  e.preventDefault()
+  mainEl.hidden =true
+  const searchTerm = inputSearch.value.trim()
+  const Url =  'https://api.unsplash.com/search/photos/?client_id=dtdf4YL-mhArMfnqr6Kff3o0ccg2LXWb6Q4bWkXEI4o&query='
+      +searchTerm
 
-  fetch(requestUrl)
+    fetch(Url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-    });
 
-  mainEl.hidden = true;
+      const results = data.results
+      for (let i = 0; i < results.length; i++) {
+      console.log(results[i].links.download);
+      
+
+   }
+
+    });
+  
 }
 
-searchBtn.addEventListener("click", getApi);
+
