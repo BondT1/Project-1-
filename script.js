c875f08f98cfe5c0a117b147b749fcfc00363635
nagibnur <nagibnur@hotmail.com>
2022-11-08 17:56:55 +0000

    API changes

--- script.js
+++ script.js
@@ -32,3 +32,22 @@ function submitHandler(e) {
 }
 
 
+fetch("https://api.pexels.com/videos/search?query=apple", {
+  headers: {
+    authorization: "563492ad6f91700001000001d0e3f25fe862425c9b3e73ffb90e2204",
+  }
+})
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  const resultsVid = data.videos;
+  for (let i = 0; i < resultsVid.length; i++) {
+    const resultsVid = data.videos[i].video_files[0].link
+    console.log(resultsVid);
+    // const imgHolder = $("#vid-holder");
+    //     const vidEl = $(`<video src=${resultsVid} class="someClass" />`);
+    //     vidHolder.append(vidEl);
+  }
+})
+ 
